1af2f464b7912af83f8288ab56abe1db88c72f3e
Ezhko <ucsd.camp.21@gmail.com>
2021-10-25 10:02:34 -0700

    fix up query

--- db/query.sql
+++ db/query.sql
@@ -10,3 +10,12 @@ on a.manager_id = b.id;
 select * from department;
 
 SELECT CONCAT(employee.first_name, " ", employee.last_name) AS name FROM employee;
+
+select role.id, title, name AS department, salary from role JOIN department on role.department_id = department.id;
+
+select
+  a.id,
+  CONCAT(a.first_name, " ", a.last_name) AS name,
+  CONCAT(b.first_name, " ", b.last_name) AS manager
+from employee AS a LEFT JOIN employee AS b
+on a.manager_id = b.id JOIN title from role on role.id = id;
--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function viewAllEmploy() {
   CONCAT(a.first_name, " ", a.last_name) AS name,
   CONCAT(b.first_name, " ", b.last_name) AS manager
 from employee AS a LEFT JOIN employee AS b
-on a.manager_id = b.id;;`
+on a.manager_id = b.id;`
     )
     .then(([rows]) => {
       console.table(rows);
